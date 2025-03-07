6019acbef3f2f358b57ab971a447b216af499dc6
Hyeri1ee <haerizian10@gmail.com>
2025-03-08 00:31:11 +0900

    feat: initial commit

---
+++ DBpool/src/main/java/hyeri/dbpool/DBpoolApplication.java
@@ -0,0 +1,13 @@
+package hyeri.dbpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBpoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBpoolApplication.class, args);
+    }
+
+}
---
+++ DBpool/src/test/java/hyeri/dbpool/DBpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package hyeri.dbpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBpoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
